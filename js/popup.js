c396ad4ea8927e289bc8b383d39c6c056ef7584f
unknown <alexeysheggi@mail.ru>
2023-02-21 03:42:24 +0100

    add new uploades

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,20 @@
 document.getElementById('homePage__button').onclick = function() {
     document.getElementById('popup').classList.add('active');
     document.getElementById('bacg-body').classList.add('active');
+    document.getElementById('body__overflow').classList.add('active');
 }
 
 document.getElementById('contacts__button').onclick = function() {
     document.getElementById('popup').classList.add('active');
     document.getElementById('bacg-body').classList.add('active');
 }
-
+body__overflow
 document.getElementById('popup__close').onclick = function(e) {
-    e.preventDefault()
+    e.preventDefault();
     document.getElementById('popup').classList.remove('active');
     document.getElementById('bacg-body').classList.remove('active');
+    document.getElementById('body__overflow').classList.remove('active');
+
 }
 
 
